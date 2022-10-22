70568d620d098d8c1d46c7c632e864c581a9383e
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-10-22 13:28:45 -0700

    fix package.json file

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require('express')
-
+const express = require('express');
+const port = 3000;
 
 const app = express();
+app.use(express.static(__dirname));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000)
+const homepagePath = __dirname + '/index.html'
+
+app.get('/', (req, res) => {
+   console.log(req.body)
+   res.sendFile(homepagePath)
+})
+
+app.listen(port, () => {
+   console.log(`philly-dip listening on port ${port} `)
+});
