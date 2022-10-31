066807eff01514cd6ca3ef9f256822a55acb82f5
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-10-31 07:30:03 -0700

    save basic data w/ express

--- index.js
+++ index.js
@@ -3,29 +3,53 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// const cors = require('cors')
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-const formpagePath = __dirname + '/public/index.html'
+const goalpagePath = __dirname + '/public/index.html'
+const petpagePath = __dirname + '/public/select-pal.html'
+const namepagePath = __dirname + '/public/name-pal.html'
 
-const mainpagePath = __dirname + '/public/home.html'
+const homepagePath = __dirname + '/public/home.html'
 
 app.use(express.static('public'));
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(formpagePath));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(goalpagePath));
     // console.log(req.body);
- })
+})
+var user = {};
+// var globaldata = require('./globaldata.js');
 
+app.post('/select-pal', (req, res) => {
+    // console.log(req.body);
+    user = req.body;
+    console.log(user)
+    res.sendFile(path.join(petpagePath), user);
+})
 
-app.post('/home',(req, res)=> {
-    console.log(req.body);
-    res.sendFile(path.join(mainpagePath));
+app.post('/home', (req, res) => {
+    if (req.body.pet) {
+        user.pet = req.body.pet
+        console.log(user)
+    }
+    res.sendFile(path.join(homepagePath));
 })
 
 
+// data test environment
+app.get("/data", (req, res) => {
+    // console.log(JSON.stringify(user))
+    res.json(user);
+})
+
+
+
 app.listen(port, () => {
-    console.log(`philly-dip listening on port ${port} `)
- });
- 
+    console.log(`philly-dips' monkey and otter server listening on port ${port} `)
+});
+
+// module.export = globaldata;
---
+++ public/app.js
@@ -0,0 +1,42 @@
+const goal = () => {
+   //fetch fake database as of right now
+   fetch('../data')
+      .then(res => res.json())
+      .then(function (user) {
+         let currentUser = user;
+         console.log(currentUser);
+         updateName(currentUser.name);
+         updatePet(currentUser.pet);
+         updateWater(currentUser.drank, currentUser.goal);
+      })
+      .catch(function(monkey) {
+         console.log(monkey)
+      })
+}
+
+goal();
+
+// const goal2 = async () => {
+//    console.log(await(await fetch('../data')).json());
+//    // let currentUser = (await (await fetch('../data')).json());
+// }
+// goal2();
+
+function updateName(name) {
+   let userName = document.querySelector('.today_goal');
+   userName.innerText = "Today's Goal for " + name
+}
+function updatePet(petType) {
+   let pet = document.querySelector('.mascot');
+   pet.src = petType + '.svg'
+}
+function updateWater(current, goal) {
+   let currentGoal = document.querySelector('.current_goal');
+   if (current == undefined) {
+      current = 0;
+   }
+   currentGoal.innerText = ` ${current}ml/${goal}ml `
+}
+
+let currentUser = {};
+
