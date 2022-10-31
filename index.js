26372fa9d4c42a4c9e22539ca2b275b909b07ad1
Pho86 <pho86@my.bcit.ca>
2022-10-31 12:55:43 -0700

    split up the pages

--- index.js
+++ index.js
@@ -10,35 +10,44 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-const goalpagePath = __dirname + '/public/index.html'
+const goalpagePath = __dirname + '/public/goal.html'
 const petpagePath = __dirname + '/public/select-pal.html'
-const namepagePath = __dirname + '/public/name-pal.html'
+const namepagePath = __dirname + '/public/index.html'
 
 const homepagePath = __dirname + '/public/home.html'
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(goalpagePath));
-    // console.log(req.body);
+    res.sendFile(path.join(namepagePath));
 })
 var user = {};
-// var globaldata = require('./globaldata.js');
 
 app.post('/select-pal', (req, res) => {
     // console.log(req.body);
-    user = req.body;
+    if (req.body) {
+        user = req.body;
+    }
     // db.run("INSERT INTO users (name, petType) values (?,?)", [name, age]);
     console.log(user)
-    res.sendFile(path.join(petpagePath), user);
+    res.sendFile(path.join(petpagePath));
+})
+
+app.post('/goal', (req, res) => {
+    if (req.body.pet) {
+        user.pet = req.body.pet;
+    }
+    // db.run("INSERT INTO users (name, petType) values (?,?)", [name, age]);
+    console.log(user);
+    res.sendFile(path.join(goalpagePath));
 })
 
 app.post('/home', (req, res) => {
     db.each("SELECT * FROM users", (err, row) => {
         console.log(row);
     });
-    if (req.body.pet) {
-        user.pet = req.body.pet
+    if (req.body.goal) {
+        user.goal = req.body.goal
         console.log(user)
     }
     res.sendFile(path.join(homepagePath));
