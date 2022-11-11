af82f38f2bab9ac6097562a5a4e39420d8295055
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-11-11 15:06:05 -0800

    finish history functionalities w/ just express and add basic leaderboard that displays every user and the current user logged in

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ let user = {};
 
 // reset the current user (aka change the current user) 
 app.post('/', (req, res) => {
-    user = {}
+    user = {};
+    history.length = 0;
     res.render("index.ejs");
 })
 
@@ -42,19 +43,19 @@ app.post('/select', (req, res) => {
         user.id = row.id;
         console.log(row);
     })
-
     res.render("select-pal.ejs", { user });
 })
 
 
 app.post('/goal', (req, res) => {
     console.log(user)
-    if (req.body.pet) {
-        user.pet = req.body.pet;
-        db.run(`UPDATE users SET pet_type=(?) WHERE name=(?)`, [user.pet, user.name]);
+    if (req.body.pet_type) {
+        user.pet_type = req.body.pet_type;
+        db.run(`UPDATE users SET pet_type=(?) WHERE name=(?)`, [user.pet_type, user.name]);
     }
     if (req.body.reset) {
         user.water_drank = 0;
+        history.length = 0;
         db.run(`UPDATE users SET water_goal=(?) WHERE name=(?)`, [user.water_drank, user.name]);
         db.run(`UPDATE users SET water_drank=(?) WHERE name=(?)`, [user.water_drank, user.name]);
     }
@@ -70,11 +71,12 @@ app.get('/home', (req, res) => {
     res.render("home.ejs", { user });
 });
 
+
 app.post('/home', (req, res) => {
-    if (req.body.goal) {
-        user.goal = req.body.goal;
+    if (req.body.water_goal) {
+        user.water_goal = req.body.water_goal;
         // console.log(user);
-        db.run(`UPDATE users SET water_goal=(?) WHERE name=(?)`, [user.goal, user.name]);
+        db.run(`UPDATE users SET water_goal=(?) WHERE name=(?)`, [user.water_goal, user.name]);
     }
     res.render("home.ejs", { user });
 })
@@ -91,8 +93,7 @@ app.get("/data", (req, res) => {
 
 
 //axios post reqeust to the data for water drank and to update the history
-app.post("/data", async (req, res) => {
-    // console.log(req.body)
+app.post("/data", (req, res) => {
     if (req.body.water_drank) {
         console.log(user.water_drank, req.body.water_drank)
         user.water_drank = req.body.water_drank;
@@ -102,32 +103,32 @@ app.post("/data", async (req, res) => {
     res.send("sending monkey data 🐒");
 })
 
-app.post("/history", async (req, res)=> {
-    console.log(req.body)
-    if (req.body.history) {
-        user.history = req.body.history;
-        // console.log(user.water_drank, req.body.water_drank); 
-    }
-    res.send("sending monkey data to history 🐒");
-})
-user.history = [];
+history = []
 app.get("/history", (req, res) => {
     res.json(user.history);
 })
 
-let scoreboard = [];
-app.get('/leaderboard', async (req, res) => {
+app.post("/history", (req, res) => {
+    history.push(req.body.history[req.body.history.length - 1]);
+    user.history = history;
+    res.send("sending monkey data to your history 🐒");
+})
+
+
+app.get('/leaderboard', (req, res) => {
+    db.run('UPDATE users SET score = ROUND((CAST([water_drank] AS FLOAT) / water_goal * 100))');
     db.all('SELECT * FROM users ORDER BY score DESC', (err, rows) => {
-        res.render("leaderboard.ejs", {'users': rows})
+        res.render("leaderboard.ejs", { 'users': rows, 'user': user })
     });
 })
-app.get('/leaderboard/api', async (req, res) => {
-    db.all('SELECT * FROM users ORDER BY water_drank / water_goal DESC', (err, rows) => {
+
+app.get('/leaderboard-score', (req, res) => {
+    db.all('SELECT * FROM users ORDER BY water_drank DESC', (err, rows) => {
         db.run('UPDATE users SET score = ROUND((CAST([water_drank] AS FLOAT) / water_goal * 100))')
-        // res.render("leaderboard.ejs", {rows})
-        res.json(rows);
+        res.render("leaderboard.ejs", { 'users': rows, 'user': user })
     });
 })
+
 
 app.listen(port, () => {
     console.log(`philly-dips' monkey and otter listening on port ${port} 🙊 🙈 🦦`);
